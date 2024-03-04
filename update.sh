02ed87116e7641f2318297a398a7dcbc55a5c723
Akki <73880884+XkshaT7678@users.noreply.github.com>
2024-03-04 21:43:14 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 cd $HOME/TermuX-Custom || exit
 
 # Pull the latest changes from the remote repository
-git pull origin master
+git pull origin main
 
 # Provide feedback to the user
 echo "Repository updated successfully!"
