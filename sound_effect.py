bca7a4ecab2c1c05bac77ddb35365b5d00645feb
Akki <73880884+XkshaT7678@users.noreply.github.com>
2024-03-04 08:54:25 +0530

    Update sound_effect.py

--- sound_effect.py
+++ sound_effect.py
@@ -3,7 +3,7 @@ import os
 num=random.randint(1,3)
 print(num)
 os.system("clear")
-print("\033[1m\033[33m[\033[32m---Booting Termux Terminal--\033[33m]\033[0m")
+print("\033[1m\033[33m[\033[32m─────────────────Booting Termux Terminal─────────────────\033[33m]\033[0m")
 
 
 # Condition 
